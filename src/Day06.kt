bbb2372e0d932f0372af15e2e087ebb89e3913cf
apocalypse87 <apocalypse87@hotmail.it>
2022-12-06 10:10:58 +0100

    solved day06

---
+++ src/Day06.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+    /**
+     * @param signal the input string containing the signal to process
+     * @param subSignalSize the size of the substring of the signal that has to be processed at time
+     * returns the index of the first packet marker or -1 if no marker is found in input signal
+     */
+    fun findStartOfPacketMarker(signal: String, subSignalSize:Int):Int{
+        for(startIndex in IntRange(0, signal.length-subSignalSize)){
+            val subSignal = signal.subSequence(startIndex, startIndex+subSignalSize)
+            if(subSignal.length == subSignal.toSet().size) return startIndex+subSignalSize
+        }
+        return -1
+    }
+
+    fun part1(input: String): Int = findStartOfPacketMarker(input, subSignalSize = 4)
+
+    fun part2(input: String): Int = findStartOfPacketMarker(input, subSignalSize = 14)
+
+
+    // verify that the solution works with test data
+    val testInput = readInputSingleLine("inputs/Day06_test")
+    println("Test Part 1: " + part1(testInput))
+    println("Test Part 2: " + part2(testInput))
+
+    //execute the two parts on the real input
+    val input = readInputSingleLine("inputs/Day06")
+    println("Part1: " + part1(input))
+    println("Part2: " + part2(input))
+}
+
+
